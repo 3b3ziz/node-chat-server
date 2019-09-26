88466c275738c9d5376fce7fc21f844723adc8cb
Amr Abdelkader <amrmak@gmail.com>
2019-09-26 18:47:25 +0200

    fix: fix chat response payload

--- routes/chats.js
+++ routes/chats.js
@@ -5,44 +5,54 @@ var Chat = require('../db/models/chat');
 /* GET all chats */
 router.get('/', function(req, res, next) {
   const requestQuery = req.query;
-  
+
   const jobTitle = requestQuery.job_title;
   const jobID = requestQuery.job_id;
-  
+
   const clientID = requestQuery.client_id;
   const clientName = requestQuery.client_name;
-  
+
   const freelancerID = requestQuery.freelancer_id;
   const freelancerName = requestQuery.freelancer_name;
 
   Chat.find({}, async (err, chats) => {
     // if query params exist, create a new chat and add it to the chats array
     // to let frontend know that this is a new chat
-    if(clientID && jobID && freelancerID) {
-      Chat.findOne({
-        job_id: jobID,
-        freelancer_id: freelancerID,
-        client_id: clientID,
-      }, (err, chat) => {
-        if (err) return console.error(err);
-        // if not found, create a new one
-        if(!chat) {
-          const newChatInstance = new Chat({
-            job_id: jobID,
-            job_title: jobTitle,
-            freelancer_id: freelancerID,
-            freelancer_name: freelancerName,
-            client_id: clientID,
-            client_name: clientName,
-          });
-          chats.unshift(newChatInstance);
-          res.json({ data: chats  });
-        } else {
-          res.json({ data: chats  });
-        }
-      });
+    if (
+      clientID &&
+      jobID &&
+      freelancerID &&
+      jobTitle &&
+      clientName &&
+      freelancerName
+    ) {
+      Chat.findOne(
+        {
+          job_id: jobID,
+          freelancer_id: freelancerID,
+          client_id: clientID,
+        },
+        (err, chat) => {
+          if (err) return console.error(err);
+          // if not found, create a new one
+          if (!chat) {
+            const newChatInstance = new Chat({
+              job_id: jobID,
+              job_title: jobTitle,
+              freelancer_id: freelancerID,
+              freelancer_name: freelancerName,
+              client_id: clientID,
+              client_name: clientName,
+            });
+            chats.unshift(newChatInstance);
+            res.json(chats);
+          } else {
+            res.json(chats);
+          }
+        },
+      );
     } else {
-      res.json({ data: chats  });
+      res.json(chats);
     }
   });
 });
@@ -53,60 +63,72 @@ router.post('/', function(req, res, next) {
 
   const jobTitle = requestBody.job_title;
   const jobID = requestBody.job_id;
-  
+
   const clientID = requestBody.client_id;
   const clientName = requestBody.client_name;
-  
+
   const freelancerID = requestBody.freelancer_id;
   const freelancerName = requestBody.freelancer_name;
 
   const message = requestBody.message;
   const senderID = requestBody.sender_id;
 
-  if(clientID && jobID && freelancerID && message && senderID) {
+  if (
+    clientID &&
+    jobID &&
+    freelancerID &&
+    jobTitle &&
+    clientName &&
+    freelancerName &&
+    message &&
+    senderID
+  ) {
     // finding the chat by jID, cID and fID
-    Chat.findOne({
-      job_id: jobID,
-      freelancer_id: freelancerID,
-      client_id: clientID,
-    }, (err, chat) => {
-      if (err) return console.error(err);
-      // if not found, create a new one
-      if(!chat) {
-        const newChatInstance = new Chat({
-          job_id: jobID,
-          job_title: jobTitle,
-          freelancer_id: freelancerID,
-          freelancer_name: freelancerName,
-          client_id: clientID,
-          client_name: clientName,
-          messages: [
-            {
-              message: message,
-              sender_id: senderID
-            }
-          ]
-        });
-        newChatInstance.save((err, chatInstance) => {
-          if (err) return console.error(err);
-          res.json({ data: chatInstance });
-        });
-      }
-      // if not, append the message to the messages of the catt
-      else {
-        chat.messages.push({
-          message: message,
-          sender_id: senderID
-        });
-        chat.save(err => {
-          if (err) return console.error(err);
-          res.status(200).json({ data: chat });
-        });
-      }
-    });
+    Chat.findOne(
+      {
+        job_id: jobID,
+        freelancer_id: freelancerID,
+        client_id: clientID,
+      },
+      (err, chat) => {
+        if (err) return console.error(err);
+        // if not found, create a new one
+        if (!chat) {
+          const newChatInstance = new Chat({
+            job_id: jobID,
+            job_title: jobTitle,
+            freelancer_id: freelancerID,
+            freelancer_name: freelancerName,
+            client_id: clientID,
+            client_name: clientName,
+            messages: [
+              {
+                message: message,
+                sender_id: senderID,
+              },
+            ],
+          });
+          newChatInstance.save((err, chatInstance) => {
+            if (err) return console.error(err);
+            res.json(chatInstance);
+          });
+        }
+        // if not, append the message to the messages of the catt
+        else {
+          chat.messages.push({
+            message: message,
+            sender_id: senderID,
+          });
+          chat.save(err => {
+            if (err) return console.error(err);
+            res.status(200).json(chat);
+          });
+        }
+      },
+    );
   } else {
-    res.status(500).json({ error: 'message' })
+    res.status(500).json({ error: 'message' });
   }
-})
+});
 
 module.exports = router;
